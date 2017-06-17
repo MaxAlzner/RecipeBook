54e3f1dd24a8a8df0e1deb0694fa1e3c54c4a625
Max Alzner <maxalzner@gmail.com>
2017-06-17 10:36:12 -0500

    base project

---
+++ Websites/WebRecipeBook/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebRecipeBook
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.validate*",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css",
+                "~/Content/Site.css"));
+        }
+    }
+}
---
+++ Websites/WebRecipeBook/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebRecipeBook
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+        }
+    }
+}
