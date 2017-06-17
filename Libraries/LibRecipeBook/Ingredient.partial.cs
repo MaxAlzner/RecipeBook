6dd2500900b67056d64264fb01ab81164f1c3147
Max Alzner <maxalzner@gmail.com>
2017-06-17 15:45:22 -0500

    correct fraction UI

---
+++ Libraries/LibRecipeBook/Ingredient.partial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Fractions;
+
+namespace LibRecipeBook
+{
+    public partial class Ingredient
+    {
+        public Fraction QuantityFraction
+        {
+            get
+            {
+                var q = Fraction.FromDecimal(this.Quantity);
+
+                if (q.Denominator == 10000)
+                {
+                    var whole = Math.Truncate(this.Quantity);
+                    if ((this.Quantity - whole) == 0.3333m)
+                    {
+                        q = new Fraction(((int)whole * 3) + 1, 3);
+                    }
+                    else if ((this.Quantity - whole) == 0.6667m)
+                    {
+                        q = new Fraction(((int)whole * 3) + 2, 3);
+                    }
+                }
+
+                return q;
+            }
+        }
+    }
+}
