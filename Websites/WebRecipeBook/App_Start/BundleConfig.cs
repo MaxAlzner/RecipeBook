6fb4cf8e9451b860a4b65d06e63e0cc2eb631d05
Max Alzner <maxalzner@gmail.com>
2017-06-17 11:22:31 -0500

    jquery updates

--- Websites/WebRecipeBook/App_Start/BundleConfig.cs
+++ Websites/WebRecipeBook/App_Start/BundleConfig.cs
@@ -8,12 +8,14 @@ namespace WebRecipeBook
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery.validate*",
+                "~/Scripts/jquery-3.2.1.js",
+                "~/Scripts/jquery.tmpl.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js",
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/bootstrap-theme.css",
                 "~/Content/Site.css"));
